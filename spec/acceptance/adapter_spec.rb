2761e49987e6b82baed6cbf9c2d12aab0c1f772f
Philipp Brüll <b.phifty@gmail.com>
2011-02-17 20:03:20 +0100

    gom-filesystem-adapter-7: Handle empty data directory

--- spec/acceptance/adapter_spec.rb
+++ spec/acceptance/adapter_spec.rb
@@ -8,4 +8,20 @@ describe "filesystem adapter" do
 
   it_should_behave_like "a read-only adapter connected to a stateless storage"
 
+  describe "setup without any data files" do
+
+    before :each do
+      GOM::Storage.teardown
+      GOM::Storage::Configuration[:test_storage].hash[:files] = "invalid"
+    end
+
+    it "should raise no exception" do
+      lambda do
+        GOM::Storage.setup
+        GOM::Storage.fetch "test_storage:object_1"
+      end.should_not raise_error
+    end
+
+  end
+
 end
