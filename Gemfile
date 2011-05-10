8fc6f147c10b9d1bc6456bce37a05e113d8ed172
Philipp Brüll <b.phifty@gmail.com>
2011-05-10 16:30:27 +0200

    updated to gom version 0.4.0

---
+++ Gemfile
@@ -0,0 +1,7 @@
+
+gem "gom", ">= 0.4.0"
+
+group :development do
+  gem "rspec", ">= 2"
+  gem "reek", ">= 1.2"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,16 @@ require 'rspec'
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "lib", "gom", "filesystem-adapter"))
 require 'gom/spec'
+
+GOM::Storage.configure {
+  storage {
+    name :test_storage
+    adapter :filesystem
+    files File.join(File.dirname(__FILE__), "acceptance", "data", "*.yml")
+    view {
+      name :test_object_class_view
+      type :class
+      model_class Object
+    }
+  }
+}
